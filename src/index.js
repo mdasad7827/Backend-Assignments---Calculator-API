68925d40556a7b3500594c14fa451fff05a955e9
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-01 18:09:50 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,145 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.post("/add", (req, res) => {
+  const myOBJ = {
+    status: "",
+    message: "",
+    sum: 0,
+  };
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    myOBJ.status = "error";
+    myOBJ.message = "Invalid data types";
+  } else {
+    const res = num1 + num2;
+    if (num1 < -1000000 || num2 < -1000000 || res < -1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Underflow";
+    } else if (num1 > 1000000 || num2 > 1000000 || res > 1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Overflow";
+    } else {
+      myOBJ.status = "success";
+      myOBJ.message = "the sum of given two numbers";
+      myOBJ.sum = res;
+    }
+  }
+  res.send(myOBJ);
+});
+
+app.post("/sub", (req, res) => {
+  const myOBJ = {
+    status: "",
+    message: "",
+    difference: 0,
+  };
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    myOBJ.status = "error";
+    myOBJ.message = "Invalid data types";
+  } else {
+    const res = num1 - num2;
+    if (num1 < -1000000 || num2 < -1000000 || res < -1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Underflow";
+    } else if (num1 > 1000000 || num2 > 1000000 || res > 1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Overflow";
+    } else {
+      myOBJ.status = "success";
+      myOBJ.message = "the difference of given two numbers";
+      myOBJ.sum = res;
+    }
+  }
+  res.send(myOBJ);
+});
+
+app.post("/multiply", (req, res) => {
+  const myOBJ = {
+    status: "",
+    message: "",
+    result: 0,
+  };
+
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    myOBJ.status = "error";
+    myOBJ.message = "Invalid data types";
+  } else {
+    const res = num1 * num2;
+    if (num1 < -1000000 || num2 < -1000000 || res < -1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Underflow";
+    } else if (num1 > 1000000 || num2 > 1000000 || res > 1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Overflow";
+    } else {
+      myOBJ.status = "success";
+      myOBJ.message = "the product of given numbers";
+      myOBJ.result = res;
+    }
+  }
+  res.send(myOBJ);
+});
+
+app.post("/divide", (req, res) => {
+  const myOBJ = {
+    status: "",
+    message: "",
+    result: 0,
+  };
+
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    myOBJ.status = "error";
+    myOBJ.message = "Invalid data types";
+  } else {
+    if (num2 === 0) {
+      myOBJ.status = "error";
+      myOBJ.message = "Cannot divide by zero";
+      break;
+    }
+
+    const res = num1 / num2;
+    if (num1 < -1000000 || num2 < -1000000 || res < -1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Underflow";
+    } else if (num1 > 1000000 || num2 > 1000000 || res > 1000000) {
+      myOBJ.status = "error";
+      myOBJ.message = "Overflow";
+    } else {
+      myOBJ.status = "success";
+      myOBJ.message = "The division of given numbers";
+      myOBJ.result = res;
+    }
+  }
+  res.send(myOBJ);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
